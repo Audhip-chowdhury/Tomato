445c6c61597ba724c2f8031901a04f8246c51766
Kushagra Sharma <77522635+randomkush@users.noreply.github.com>
2026-05-24 05:42:29 +0530

    Revert "Add order tracking and checkout functionality"

--- tomato-frontend/src/App.jsx
+++ tomato-frontend/src/App.jsx
@@ -7,7 +7,6 @@ import Home from './pages/Home'
 import Restaurants from './pages/Restaurants'
 import RestaurantDetail from './pages/RestaurantDetail'
 import Cart from './pages/Cart'
-import OrderTracking from './pages/OrderTracking'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import useAuthStore from './store/authStore'
@@ -36,14 +35,6 @@ export default function App() {
             element={
               <ProtectedRoute>
                 <Cart />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/orders/:orderId/track"
-            element={
-              <ProtectedRoute>
-                <OrderTracking />
               </ProtectedRoute>
             }
           />
